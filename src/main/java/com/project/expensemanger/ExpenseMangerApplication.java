442637e8daf06b3f1081d2b84bd591fba92ae3cf
ahj30420 <ahj30420@naver.com>
2025-11-10 18:36:35 +0900

    feat: 프로젝트 기본 세팅

---
+++ src/main/java/com/project/expensemanger/ExpenseMangerApplication.java
@@ -0,0 +1,13 @@
+package com.project.expensemanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseMangerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/expensemanger/core/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.project.expensemanger.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Expense_Manager")
+                .packagesToScan("com.project.expense_manager")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Expense_Manager")
+                        .description("예산 관리 시스템")
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Development Server")
+                ));
+    }
+}
