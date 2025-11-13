25956986752a56977cc8fc26f512fbd58dffbb8d
ahj30420 <ahj30420@naver.com>
2025-11-13 18:40:10 +0900

    feat: 예산 관련 예외 코드 추가

---
+++ src/main/java/com/project/expensemanger/core/common/exception/errorcode/BudgetErrorCode.java
@@ -0,0 +1,22 @@
+package com.project.expensemanger.core.common.exception.errorcode;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum BudgetErrorCode implements ErrorCode {
+    BUDGET_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 예산이 존재합니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
