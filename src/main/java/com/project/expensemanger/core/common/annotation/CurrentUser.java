c14bed0219a94d43ee3940ee85a18eeba4b7e64f
ahj30420 <ahj30420@naver.com>
2025-11-13 18:40:34 +0900

    feat: @CurrentUser 어노테이션 추가하여 인증 사용자 ID 주입 처리

---
+++ src/main/java/com/project/expensemanger/core/common/annotation/CurrentUser.java
@@ -0,0 +1,13 @@
+package com.project.expensemanger.core.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? 0L : #this")
+public @interface CurrentUser {
+}
