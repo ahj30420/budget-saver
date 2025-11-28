36dbdd468c477868ed0b9bce9f5053ad08d3eb9d
ahj30420 <ahj30420@naver.com>
2025-11-28 15:03:17 +0900

    feat: 랜덤 더미 데이터 생성 프로시저 추가

--- sql/insert_dummy_data.sql
+++ sql/insert_dummy_data.sql
@@ -119,6 +119,37 @@ BEGIN
     END WHILE;
 END //
 
+CREATE PROCEDURE create_dummy_expenditure_random(total_rows INT)
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    DECLARE random_user INT;
+    DECLARE random_category INT;
+    DECLARE random_amount INT;
+    DECLARE random_date DATE;
+
+    WHILE i <= total_rows DO
+        -- user_id 랜덤 (예: 1~10)
+        SET random_user = FLOOR(1 + RAND() * 10);
+
+        -- category_id 랜덤 (예: 1~7)
+        SET random_category = FLOOR(1 + RAND() * 7);
+
+        -- amount 랜덤 (1000 단위, 1~10 * 1000)
+        SET random_amount = FLOOR(1 + RAND() * 10) * 1000;
+
+        -- spend_at 랜덤 날짜 (2024-01-01 ~ 2025-12-31)
+        SET random_date = DATE_ADD('2024-01-01', INTERVAL FLOOR(RAND() * 730) DAY);
+
+        INSERT INTO expenditure(
+            spend_at, category_id, user_id, amount, excluded_from_total, memo, is_deleted, created_at, updated_at
+        ) VALUES (
+            random_date, random_category, random_user, random_amount, FALSE, CONCAT('메모', i), FALSE, NOW(), NOW()
+        );
+
+        SET i = i + 1;
+    END WHILE;
+END //
+
 
 DELIMITER ;
 
@@ -136,3 +167,4 @@ VALUES
 CALL create_dummy_user();
 CALL create_dummy_budget('2025');
 CALL create_dummy_expenditure('2025');
+CALL create_dummy_expenditure_random(100000);
