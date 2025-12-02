efbbde42f827d152e025a8dc1fcbda21e601f1cc
ahj30420 <ahj30420@naver.com>
2025-12-02 22:17:23 +0900

    test: 오늘 지출 안내 기능 service 테스트 추가

--- src/test/java/com/project/expensemanger/manager/application/service/ExpenditureConsultingServiceTest.java
+++ src/test/java/com/project/expensemanger/manager/application/service/ExpenditureConsultingServiceTest.java
@@ -7,12 +7,17 @@ import static org.mockito.BDDMockito.given;
 
 import com.project.expensemanger.manager.application.mock.BudgetMock;
 import com.project.expensemanger.manager.application.mock.CategoryBudgetUsageMock;
+import com.project.expensemanger.manager.application.mock.ExpenditureMock;
 import com.project.expensemanger.manager.application.port.out.BudgetPort;
 import com.project.expensemanger.manager.application.port.out.ConsultingPort;
+import com.project.expensemanger.manager.application.port.out.ExpenditurePort;
 import com.project.expensemanger.manager.application.service.dto.GetBudgetUsageCondition;
 import com.project.expensemanger.manager.application.service.model.CategoryRecommendtaionModel;
 import com.project.expensemanger.manager.application.service.model.TodayBudgetRecommendationModel;
+import com.project.expensemanger.manager.application.service.model.TodayCategoryExpenditureModel;
+import com.project.expensemanger.manager.application.service.model.TodayExpenditureSummaryModel;
 import com.project.expensemanger.manager.domain.consulting.CategoryBudgetUsage;
+import com.project.expensemanger.manager.domain.expenditure.Expenditure;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,8 +38,12 @@ class ExpenditureConsultingServiceTest {
     @Mock
     private ConsultingPort consultingPort;
 
+    @Mock
+    private ExpenditurePort expenditurePort;
+
     private BudgetMock budgetMock = new BudgetMock();
     private CategoryBudgetUsageMock categoryBudgetUsageMock = new CategoryBudgetUsageMock();
+    private ExpenditureMock expenditureMock = new ExpenditureMock();
 
     @Test
     @DisplayName("오늘 지출 추천 로직 테스트")
@@ -52,16 +61,16 @@ class ExpenditureConsultingServiceTest {
 
         // then
         assertThat(result).isNotNull();
-        assertThat(result.getCategories()).hasSize(1);
-        assertThat(result.getTodayTotalPossible()).isGreaterThan(0);
-        assertThat(result.getMessage()).isNotEmpty();
+        assertThat(result.categories()).hasSize(1);
+        assertThat(result.todayTotalPossible()).isGreaterThan(0);
+        assertThat(result.message()).isNotEmpty();
 
-        CategoryRecommendtaionModel categoryRecommendation = result.getCategories().get(0);
-        assertThat(categoryRecommendation.getCategoryId()).isEqualTo(
+        CategoryRecommendtaionModel categoryRecommendation = result.categories().get(0);
+        assertThat(categoryRecommendation.categoryId()).isEqualTo(
                 categoryBudgetUsageMock.domainMock().getCategoryId());
-        assertThat(categoryRecommendation.getCategoryName()).isEqualTo(
+        assertThat(categoryRecommendation.categoryName()).isEqualTo(
                 categoryBudgetUsageMock.domainMock().getCategoryName());
-        assertThat(categoryRecommendation.getRecommendedAmount()).isGreaterThan(0L);
+        assertThat(categoryRecommendation.recommendedAmount()).isGreaterThan(0L);
     }
 
     @Test
@@ -77,8 +86,54 @@ class ExpenditureConsultingServiceTest {
 
         // then
         assertThat(result).isNotNull();
-        assertThat(result.getCategories()).isEmpty();
-        assertThat(result.getTodayTotalPossible()).isEqualTo(0L);
-        assertThat(result.getMessage()).isEmpty();
+        assertThat(result.categories()).isEmpty();
+        assertThat(result.todayTotalPossible()).isEqualTo(0L);
+        assertThat(result.message()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("오늘 지출 안내 로직 테스트")
+    void get_today_expenditure_test() {
+        // given
+        Long userId = budgetMock.getUserId();
+
+        List<Expenditure> todayExpenditures = List.of(expenditureMock.domainMock());
+        List<CategoryBudgetUsage> categoryUsageList = List.of(categoryBudgetUsageMock.domainMock());
+
+        given(budgetPort.findLastestBudget(anyLong())).willReturn(budgetMock.domainMock());
+        given(expenditurePort.findTodayExpenditure(anyLong())).willReturn(todayExpenditures);
+        given(consultingPort.getCategoryBudgetUsage(any())).willReturn(categoryUsageList);
+
+        // when
+        TodayExpenditureSummaryModel result = sut.getTodayExpenditure(userId);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.totalTodayAmount()).isGreaterThan(0);
+        assertThat(result.categories()).hasSize(1);
+
+        TodayCategoryExpenditureModel categoryModel = result.categories().get(0);
+        assertThat(categoryModel.categoryId()).isEqualTo(categoryBudgetUsageMock.domainMock().getCategoryId());
+        assertThat(categoryModel.categoryName()).isEqualTo(categoryBudgetUsageMock.domainMock().getCategoryName());
+        assertThat(categoryModel.todayAmount()).isGreaterThanOrEqualTo(0);
+        assertThat(categoryModel.todayRecommendedAmount()).isGreaterThanOrEqualTo(0);
+        assertThat(categoryModel.riskPercentage()).isGreaterThanOrEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("오늘 지출 안내 로직 테스트 : 예산 없음 또는 지출 데이터 없음")
+    void get_today_expenditure_empty_test() {
+        // given
+        Long userId = budgetMock.getUserId();
+
+        given(budgetPort.findLastestBudget(anyLong())).willReturn(null);
+
+        // when
+        TodayExpenditureSummaryModel result = sut.getTodayExpenditure(userId);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.totalTodayAmount()).isEqualTo(0L);
+        assertThat(result.categories()).isEmpty();
     }
 }
